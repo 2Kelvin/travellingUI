95e8bb72aa75fd4b51893f08925878b6e488583a
2Kelvin <rocketmancodes02@gmail.com>
2024-08-19 18:41:40 +0300

    add profile section

--- app/src/main/java/com/example/travellingui/ui/screens/Home.kt
+++ app/src/main/java/com/example/travellingui/ui/screens/Home.kt
@@ -1,7 +1,81 @@
 package com.example.travellingui.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.travellingui.R
+import com.example.travellingui.ui.theme.TravellingUITheme
 
+/** Home screen */
 @Composable
-fun Home(modifier: Modifier = Modifier) {}
+fun Home(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 12.dp, vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+
+        // hello Jonny
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row {
+                Text(
+                    text = "Hello, ",
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Medium
+                )
+                Text(
+                    text = "Johny",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 28.sp
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.prof),
+                    contentDescription = "profile picture",
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+
+        // search
+    }
+}
+
+/** PREVIEWS */
+@Preview
+@Composable
+fun HomePreview() {
+    TravellingUITheme {
+        Home()
+    }
+}
