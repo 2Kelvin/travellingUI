037bd76badaabdce8bd7c2e23c641e7a1f58f6ef
2Kelvin <rocketmancodes02@gmail.com>
2024-08-19 17:16:37 +0300

    first commit

---
+++ app/src/main/java/com/example/travellingui/ui/screens/Details.kt
@@ -0,0 +1,2 @@
+package com.example.travellingui.ui.screens
+
---
+++ app/src/main/java/com/example/travellingui/ui/screens/Home.kt
@@ -0,0 +1,7 @@
+package com.example.travellingui.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Home(modifier: Modifier = Modifier) {}
